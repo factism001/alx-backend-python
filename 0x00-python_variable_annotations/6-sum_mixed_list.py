8740effb1a99d6c59bcc823b303113c0914fcce5
factism <factism001@gmail.com>
2023-08-04 11:27:38 +0100

    corrected pycodestyle error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Calculates the sum of a mixed int and float list.
 
-This module provides a function to calculate the total sum of 
+This module provides a function to calculate the total sum of
 a list containing integers and floats.
 """
 
@@ -9,16 +9,15 @@ from typing import List, Union
 
 MixedList = List[Union[int, float]]
 
+
 def sum_mixed_list(mxd_lst: MixedList) -> float:
     """Sums the values in a mixed int and float list.
-    
     Args:
-        mxd_lst (List[Union[int, float]]): The mixed list. 
+        mxd_lst (List[Union[int, float]]): The mixed list.
 
     Returns:
         float: The total sum of the list values as a float.
-    """
-    
+        """
     total = 0
     for num in mxd_lst:
         total += num
