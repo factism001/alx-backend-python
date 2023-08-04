43673581f356dcf1dc63aef6ed203f9c25d76e01
factism <factism001@gmail.com>
2023-08-04 10:43:16 +0100

    corrected pycodestyle error

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
-"""Concatenates two strings together. 
+"""Concatenates two strings together.
 
-This module provides a function for concatenating two string values 
+This module provides a function for concatenating two string values
 and returning the concatenated string.
 """
 
-from typing import TypeVar
 
-String = TypeVar('String', str)
-
-def concat(str1: String, str2: String) -> String:
+def concat(str1: str, str2: str) -> str:
     """Concatenates two strings together.
-    
+
     Args:
         str1: The first string.
         str2: The second string.
 
     Returns:
         A string containing str1 + str2.
-    """
-    
+
+        """
     return str1 + str2
