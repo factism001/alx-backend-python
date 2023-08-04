e5feabb5cef3565508fb4066b945ec99829c2caf
factism <factism001@gmail.com>
2023-08-04 11:16:40 +0100

    corrected pycodestyle error

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """Calculates the sum of a list of floats.
 
-This module provides a function for finding the total sum of a 
-list containing float values. 
+This module provides a function for finding the total sum of a
+list containing float values.
 """
 
 from typing import List
 
 FloatNum = List[float]
 
+
 def sum_list(input_list: FloatNum) -> float:
     """Calculates the sum of a list of floats.
-    
     Args:
         input_list (List[float]): The list of floats to sum.
 
