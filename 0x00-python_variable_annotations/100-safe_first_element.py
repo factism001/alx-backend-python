f907439e852d406cdc84415befaeee901043c957
factism <factism001@gmail.com>
2023-08-04 11:51:44 +0100

    corrected pycodestyle error

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,22 @@
-from typing import Sequence, Optional
+#!/usr/bin/env python3
+"""Augment the following code with
+the correct duck-typed annotations
+"""
 
-# The types of the elements of the input are not known
-def safe_first_element(lst: Sequence[object]) -> Optional[object]:
+from typing import Union
+
+
+def safe_first_element(lst) -> Union[None, int, float, str, list, tuple, dict]:
+    """
+    Returns the first element of the input list or None if the list is empty.
+
+    Args:
+        lst: A list that can contain elements of any type.
+
+    Returns:
+        Union[None, int, float, str, list, tuple, dict, set]:
+        The first element of the list or None if the list is empty.
+    """
     if lst:
         return lst[0]
     else:
