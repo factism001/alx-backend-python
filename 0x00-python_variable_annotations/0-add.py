1e13cbd02059dda4adb71bf2b3778273997d79f7
factism <factism001@gmail.com>
2023-08-04 10:24:30 +0100

    corrected pycodestyle error

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,18 +5,19 @@ This module provides a function to add two float values together
 and return the result as a float.
 """
 
-from typing import TypeVar
+from typing import TypeVar Union
 
-FloatNumber = TypeVar('FloatNumber', float)
+FloatNumber = TypeVar('FloatNumber', float, Union[float, int])
+
 
 def add(a: FloatNumber, b: FloatNumber) -> FloatNumber:
     """Adds two float values together.
-    
+
     Args:
         a: First float to add.
         b: Second float to add.
-        
-    Returns:
+
+        Returns:
         The sum of a and b as a float.
     """
 
