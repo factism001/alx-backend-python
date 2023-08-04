5e645106c9473779c1ff11616fb583f751c36860
factism <factism001@gmail.com>
2023-08-04 11:34:46 +0100

    corrected pycodestyle error

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,14 +5,14 @@ from typing import Callable
 
 FloatNum = float
 
+
 def make_multiplier(multiplier: FloatNum) -> Callable[[FloatNum], FloatNum]:
     """Creates a function that multiplies by a multiplier.
-    
     Args:
         multiplier (float): The number to multiply by.
 
     Returns:
-        A function that takes a float and returns 
+        A function that takes a float and returns
         the product with multiplier.
     """
 
